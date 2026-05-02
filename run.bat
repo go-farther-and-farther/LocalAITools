9ae6f8437dee502194eb2189c332a054acf24fe1
LQL <2859167710@qq.com>
2026-05-02 19:15:51 +0800

    V1.3 版本修复了启动脚本BUG

--- run.bat
+++ run.bat
@@ -2,9 +2,9 @@
 chcp 65001 >nul
 cd /d "%~dp0"
 echo ============================================
-echo   LocalAITools - 本地 AI 工具箱
-echo   正在启动...
+echo   LocalAITools
+echo   Starting...
 echo ============================================
 echo.
-python app.py
+.venv\Scripts\python app.py
 pause
